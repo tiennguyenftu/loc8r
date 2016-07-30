d292a9a2d80a09b09ae5efbd1cbfd000e4b774ec
Tien Nguyen <tiennguyen.ftu.k52@gmail.com>
2016-07-30 20:41:13 +0700

    Finish prototype static website

--- app-server/controllers/locations.js
+++ app-server/controllers/locations.js
@@ -1,11 +1,43 @@
 /* GET Home page. */
 module.exports.homeList = function(req, res) {
-    res.render('location-list', { title: 'Home' });
+    res.render('locations-list', {
+        title: 'Loc8r - find a place to work with wifi',
+        pageHeader: {
+            title: 'Loc8r',
+            strapLine: 'Find places to works with wifi near you!'
+        },
+        sidebar: "Looking for wifi and a seat? Loc8r helps you find places to work when out and about. Perhaps with coffee, cake or a pint? Let Loc8r help you find the place you're looking for.",
+        locations: [
+            {
+                name: 'Starcups',
+                address: '125 High Street, Reading, RG6 1PS',
+                rating: 3,
+                facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+                distance: '100m'
+            },
+            {
+                name: 'Cafe Hero',
+                address: '125 High Street, Reading, RG6 1PS',
+                rating: 4,
+                facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+                distance: '200m'
+            },
+            {
+                name: 'Starcups',
+                address: '125 High Street, Reading, RG6 1PS',
+                rating: 2,
+                facilities: ['Food', 'Premium wifi'],
+                distance: '250m'
+            }
+        ]
+    });
 };
 
 /* GET Location Info page. */
 module.exports.locationInfo = function(req, res) {
-    res.render('location-info', { title: 'Location info' });
+    res.render('location-info', {
+        title: 'Location Info'
+    });
 };
 
 /* GET  page. */
